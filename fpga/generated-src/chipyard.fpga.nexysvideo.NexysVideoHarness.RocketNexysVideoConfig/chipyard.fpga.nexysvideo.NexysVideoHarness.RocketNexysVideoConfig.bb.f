438b9ac303f6252d3bcaba53ab0a2272750e8def
James Lukas <jameslukas@berkeley.edu>
2025-06-20 12:14:05 -0700

    adding changed files

---
+++ fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig.bb.f
@@ -0,0 +1,6 @@
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/AnalogToUInt_1.v
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/GenericDigitalInIOCell.v
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/GenericDigitalOutIOCell.v
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/PowerOnResetFPGAOnly.v
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/UIntToAnalog_1.v
+/bwrcq/C/junsun/nexys_video_chipyard/fpga/generated-src/chipyard.fpga.nexysvideo.NexysVideoHarness.RocketNexysVideoConfig/gen-collateral/plusarg_reader.v
